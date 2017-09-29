059c19493bdd7c153a645fbbfd3cb70268f69ce4
Celine BEGNE <cbegne@e1r9p5.42.fr>
2017-09-29 17:19:59 +0200

    style(movie): style movie page

--- app/src/Movie/containers/Movie.js
+++ app/src/Movie/containers/Movie.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Loading from '../../General/components/Loading';
 import TorrentTable from '../components/TorrentTable';
 import '../css/movie.css';
 
@@ -7,24 +8,27 @@ const movie = {
   torrents: [
     {
       "url":"https://yts.ag/torrent/download/3549148407CA28A6069380F9F91C01DD584255A7",
+      "magnet":"",
+      "title":"",
       "hash":"3549148407CA28A6069380F9F91C01DD584255A7",
       "quality":"720p",
+      "size":"665.4 MB",
       "seeds":620,
       "peers":1094,
-      "size":"665.4 MB",
-      "size_bytes":697722470,
+      "source": "yifi",
     },
     {
       "url":"https://yts.ag/torrent/download/0FA39CA32B1406725191E07109309A4D9E1F801E",
+      "magnet":"",
+      "title":"",
       "hash":"0FA39CA32B1406725191E07109309A4D9E1F801E",
       "quality":"1080p",
       "seeds":583,
       "peers":808,
       "size":"1.38 GB",
-      "size_bytes":1481763717,
-      "date_uploaded":"2017-09-28 08:00:53",
-      "date_uploaded_unix":1506600053}
-    ], //yify
+      "source": "yifi",
+    }
+  ],
   title: ["Wish Upon", "I Wish - Faites un vœu"], //tmdb
   year: 2017, //tmdb
   overview: [
@@ -43,6 +47,8 @@ const movie = {
   thumb: "https://images-na.ssl-images-amazon.com/images/M/MV5BOGQxN2NlMWItNzMyOC00ODYxLThkNDktMWQ0ZjA2MjQyYjIwXkEyXkFqcGdeQXVyMjM4NTM5NDY@._V1_UX182_CR0,0,182,268_AL_.jpg",
 }
 
+const lang = 1; // lang = 0 => english, lang = 1 = francais
+
 const timing = (length) => {
   const hours = Math.trunc(length / 60);
   const minutes = length % 60;
@@ -52,7 +58,7 @@ const timing = (length) => {
 class Movie extends Component {
 
   state = {
-
+    loaded: false,
   }
 
   componentDidMount() {
@@ -62,60 +68,70 @@ class Movie extends Component {
     //   if (error) {
     //     this.setState({ error });
     //   } else {
-    //     this.user = user;
+    //     this.movie = movie;
     //     this.setState({
-    //       movie,
+    //       loaded: true,
     //     });
     //   }
     // });
   }
 
   render() {
-    const genres = movie.genres[0].map((genre, index, array) => {
+    // const { loaded } = this.state;
+    // if (loaded === false) { return <Loading />; }
+    const genres = movie.genres[lang].map((genre, index, array) => {
       if (index === array.length - 1) {
-        return (<span>{genre}</span>);
+        return (<span key={genre}>{genre}</span>);
       }
-      return (<span>{genre}, </span>);
+      return (<span key={genre}>{genre}, </span>);
     });
     const actors = movie.cast.map((actor, index, array) => {
       if (index === array.length - 1) {
-        return (<span>{actor}</span>);
+        return (<span key={actor}>{actor}</span>);
       }
-      return (<span>{actor}, </span>);
+      return (<span key={actor}>{actor}, </span>);
     });
     const timeObj = timing(movie.length);
     const time = `${timeObj.hours}h${timeObj.minutes}`;
 
     return (
       <div className="movie-container">
-        <img className="movie-poster" src={movie.posterLarge} alt="" />
-        <div>
-          <h1>{ movie.title[0] }</h1>
+        <img className="movie-poster" src={movie.posterLarge} alt="movie" />
+        <div className="movie-infos">
+          <h1>{ movie.title[lang] }</h1>
           <div>
-            <span>{ movie.year } </span>
+            <span className="movie-year">{ movie.year }</span>
             <span>{ time } </span>
-            <span>IMDB: { movie.rating }/10</span>
           </div>
           <div>
-            Overview: { movie.overview[0] }
+            <span>
+              <i className="glyphicon glyphicon-star" />
+              <span className="movie-rating">{ movie.rating }</span>
+              <span>{'/'}10</span>
+            </span>
+          </div>
+          <div className="movie-overview">
+            { movie.overview[lang] }
           </div>
           <div className="movie-details">
             <div>
-              With: { actors }
+              <span className="movie-details-text">With:</span>
+              { actors }
             </div>
             <div>
-              Director: { movie.director }
+              <span className="movie-details-text">Director:</span>
+              { movie.director }
             </div>
             <div>
-              Genres: { genres }
+              <span className="movie-details-text">Genres:</span>
+              { genres }
             </div>
           </div>
-          <TorrentTable movie={movie} />
+          <TorrentTable movie={movie} lang={lang} />
         </div>
       </div>
     );
   }
-
 }
 
 export default Movie;
