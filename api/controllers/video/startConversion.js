78ea4a1f0717c33027f501b701634234583c391a
Robin Monnier <monnier.robin@gmail.com>
2017-10-18 11:48:18 +0200

    feat(startConversion): fix mkv conversion

--- api/controllers/video/startConversion.js
+++ api/controllers/video/startConversion.js
@@ -18,12 +18,15 @@ const startConversion = (torrent, fileStream, res) => new Promise((resolve, reje
   data.oldPath = data.path;
   data.path = `${data.oldPath}.converted.mp4`;
 
-  const converter = ffmpeg().input(fileStream);
   console.log('spiderStreamer Notice: Starting conversion to video/mp4 for ', data.oldPath);
-  converter.on('error', (err, stdout, stderr) => {
-    console.error('spiderStreamer Error:'.red, 'Could not convert file:', data.oldPath);
-    console.log('fluent-ffmpeg Error:'.red, '\nErr:', err, '\nStdOut:', stdout, '\nStdErr:', stderr);
-  })
+  const converter = ffmpeg()
+    .input(fileStream)
+    .outputOptions('-movflags frag_keyframe+empty_moov')
+    .outputFormat('mp4')
+    .on('error', (err, stdout, stderr) => {
+      console.error('spiderStreamer Error:'.red, 'Could not convert file:', data.oldPath);
+      console.log('fluent-ffmpeg Error:'.red, '\nErr:', err, '\nStdOut:', stdout, '\nStdErr:', stderr);
+    })
     .on('start', (cmd) => {
       console.log('fluent-ffmpeg Notice: Started:', cmd);
     })
@@ -45,8 +48,7 @@ const startConversion = (torrent, fileStream, res) => new Promise((resolve, reje
     converter.output(res);
   }
   if (fileExtension === '.mkv') {
-    converter.inputFormat('.mkv')
-      .addOption('-vcodec')
+    converter.addOption('-vcodec')
       .addOption('copy')
       .addOption('-acodec')
       .addOption('copy')
@@ -55,8 +57,6 @@ const startConversion = (torrent, fileStream, res) => new Promise((resolve, reje
     converter.inputFormat(fileExtension.substr(1))
       .audioCodec('aac')
       .videoCodec('libx264')
-      .outputFormat('mp4')
-      .outputOptions('-movflags frag_keyframe+empty_moov')
       .run();
   }
 });
